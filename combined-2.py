8b338a85dfb1667dd08611622bea7900d25d8775
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2023-09-10 17:58:43 +0530

    init

--- combined-2.py
+++ combined-2.py
@@ -5,13 +5,16 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-bicep_curl = True
-deadlift = False
+# Initialize variables for squat and bicep curl detection
+squat_counter = 0
+squat_stage = None
+bicep_curl_counter = 0
+bicep_curl_stage = None
 
 def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
 
     radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
     angle = np.abs(radians * 180.0 / np.pi)
@@ -22,11 +25,6 @@ def calculate_angle(a, b, c):
     return angle
 
 cap = cv2.VideoCapture(0)
-
-counter_curl = 0
-stage_curl = None
-counter_deadlift = 0
-stage_deadlift = None
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
@@ -43,101 +41,84 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Extract landmarks for Bicep Curl and Deadlift
-        if bicep_curl:
-            try:
-                landmarks = results.pose_landmarks.landmark
+        # Extract landmarks for squat and bicep curl detection
+        try:
+            landmarks = results.pose_landmarks.landmark
 
-                # Get coordinates for bicep curl
-                shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            # Get coordinates for squat detection
+            hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                   landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+            ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
 
-                # Calculate angle for bicep curl
-                angle = calculate_angle(shoulder, elbow, wrist)
+            # Get coordinates for bicep curl detection
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
-                # Visualize angle for bicep curl
-                cv2.putText(image, f'Bicep Curl Angle: {angle:.2f} degrees',
-                            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+            # Calculate knee joint angle for squat
+            angle_knee = calculate_angle(hip, knee, ankle)
 
-                # Curl counter logic for bicep curl
-                if angle > 160:
-                    stage_curl = "down"
-                if angle < 30 and stage_curl == 'down':
-                    stage_curl = "up"
-                    counter_curl += 1
-                    print(f'Bicep Curl Count: {counter_curl}')
+            # Calculate angle for bicep curl
+            angle_bicep_curl = calculate_angle(shoulder, elbow, wrist)
 
-            except:
-                bicep_curl = False
-                pass
+            # Visualize knee angle for squat detection
+            cv2.putText(image, f'Knee Angle: {angle_knee:.2f} degrees',
+                        (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (79, 121, 66), 2, cv2.LINE_AA)
 
-        if deadlift:
-            try:
-                landmarks = results.pose_landmarks.landmark
+            # Visualize angle for bicep curl detection
+            cv2.putText(image, f'Bicep Curl Angle: {angle_bicep_curl:.2f} degrees',
+                        (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
 
-                # Get coordinates for deadlift
-                hips = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-                knees = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
-                         landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
-                ankles = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
-                          landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+            # Squat counter logic
+            if angle_knee > 169:
+                squat_stage = "UP"
+            if angle_knee <= 90 and squat_stage == 'UP':
+                squat_stage = "DOWN"
+                squat_counter += 1
+                print(f'Squat Count: {squat_counter}')
 
-                # Calculate angle for deadlift
-                angle = calculate_angle(hips, knees, ankles)
+            # Bicep curl counter logic
+            if angle_bicep_curl > 160:
+                bicep_curl_stage = "down"
+            if angle_bicep_curl < 30 and bicep_curl_stage == 'down':
+                bicep_curl_stage = "up"
+                bicep_curl_counter += 1
+                print(f'Bicep Curl Count: {bicep_curl_counter}')
 
-                # Visualize angle for deadlift
-                cv2.putText(image, f'Deadlift Angle: {angle:.2f} degrees',
-                            (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        except:
+            pass
 
-                # Deadlift counter logic
-                if angle > 120:
-                    stage_deadlift = "down"
-                if angle < 60 and stage_deadlift == 'down':
-                    stage_deadlift = "up"
-                    counter_deadlift += 1
-                    print(f'Deadlift Count: {counter_deadlift}')
+        # Render squat and bicep curl counters and stages
+        cv2.rectangle(image, (0, 0), (225, 120), (245, 117, 16), -1)
 
-            except:
-                deadlift = False
-                pass
-
-        # Render curl and deadlift counters
-        cv2.putText(image, 'Exercise: Bicep Curl', (10, 12),
+        cv2.putText(image, 'SQUAT REPS', (15, 12),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, 'Exercise: Deadlift', (10, 42),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-
-        # Rep data for bicep curl
-        cv2.putText(image, 'Bicep Curl Reps', (15, 80),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter_curl),
-                    (10, 120),
+        cv2.putText(image, str(squat_counter),
+                    (10, 60),
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
-        # Rep data for deadlift
-        cv2.putText(image, 'Deadlift Reps', (15, 160),
+        cv2.putText(image, 'SQUAT STAGE', (65, 12),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter_deadlift),
-                    (10, 200),
+        cv2.putText(image, squat_stage,
+                    (60, 60),
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
-        # Stage data for bicep curl
-        cv2.putText(image, 'Bicep Curl Stage', (120, 80),
+        cv2.putText(image, 'BICEP CURL REPS', (15, 100),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage_curl,
-                    (110, 120),
+        cv2.putText(image, str(bicep_curl_counter),
+                    (10, 140),
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
-        # Stage data for deadlift
-        cv2.putText(image, 'Deadlift Stage', (120, 160),
+        cv2.putText(image, 'BICEP CURL STAGE', (65, 100),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage_deadlift,
-                    (110, 200),
+        cv2.putText(image, bicep_curl_stage,
+                    (60, 140),
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Render detections
