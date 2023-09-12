646e424d6a79cccb2b7f9fe2beebfcfc781ffff1
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2023-09-12 22:54:45 +0530

    squat

--- combined-2.py
+++ combined-2.py
@@ -1,3 +1,5 @@
+# squats may be detected as curls
+
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -5,11 +7,9 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# Initialize variables for squat and bicep curl detection
+# Initialize variables for squat detection
 squat_counter = 0
 squat_stage = None
-bicep_curl_counter = 0
-bicep_curl_stage = None
 
 def calculate_angle(a, b, c):
     a = np.array(a)
@@ -41,7 +41,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Extract landmarks for squat and bicep curl detection
+        # Extract landmarks for squat detection
         try:
             landmarks = results.pose_landmarks.landmark
 
@@ -53,27 +53,12 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
                      landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
 
-            # Get coordinates for bicep curl detection
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-
-            # Calculate knee joint angle for squat
+            # Calculate knee joint angle
             angle_knee = calculate_angle(hip, knee, ankle)
-
-            # Calculate angle for bicep curl
-            angle_bicep_curl = calculate_angle(shoulder, elbow, wrist)
 
             # Visualize knee angle for squat detection
             cv2.putText(image, f'Knee Angle: {angle_knee:.2f} degrees',
                         (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (79, 121, 66), 2, cv2.LINE_AA)
-
-            # Visualize angle for bicep curl detection
-            cv2.putText(image, f'Bicep Curl Angle: {angle_bicep_curl:.2f} degrees',
-                        (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
 
             # Squat counter logic
             if angle_knee > 169:
@@ -83,19 +68,11 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                 squat_counter += 1
                 print(f'Squat Count: {squat_counter}')
 
-            # Bicep curl counter logic
-            if angle_bicep_curl > 160:
-                bicep_curl_stage = "down"
-            if angle_bicep_curl < 30 and bicep_curl_stage == 'down':
-                bicep_curl_stage = "up"
-                bicep_curl_counter += 1
-                print(f'Bicep Curl Count: {bicep_curl_counter}')
-
         except:
             pass
 
-        # Render squat and bicep curl counters and stages
-        cv2.rectangle(image, (0, 0), (225, 120), (245, 117, 16), -1)
+        # Render squat counter
+        cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
 
         cv2.putText(image, 'SQUAT REPS', (15, 12),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
@@ -107,18 +84,6 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
         cv2.putText(image, squat_stage,
                     (60, 60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        cv2.putText(image, 'BICEP CURL REPS', (15, 100),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(bicep_curl_counter),
-                    (10, 140),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-
-        cv2.putText(image, 'BICEP CURL STAGE', (65, 100),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-        cv2.putText(image, bicep_curl_stage,
-                    (60, 140),
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Render detections
--- combined.py
+++ combined.py
@@ -1,152 +1,134 @@
-# Has errrors
+#Not working properly
 
 
 import cv2
 import mediapipe as mp
 import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# Define landmarks for both exercises
-exercise_landmarks = {
-    "Bicep Curl": {
-        "shoulder": mp_pose.PoseLandmark.LEFT_SHOULDER,
-        "elbow": mp_pose.PoseLandmark.LEFT_ELBOW,
-        "wrist": mp_pose.PoseLandmark.LEFT_WRIST,
-    },
-    "Leg Raise": {
-        "hip": mp_pose.PoseLandmark.LEFT_HIP,
-        "knee": mp_pose.PoseLandmark.LEFT_KNEE,
-        "ankle": mp_pose.PoseLandmark.LEFT_ANKLE,
-    },
-}
-
-angle_thresholds = {
-    "Bicep Curl": {"up": 30, "down": 160},
-    "Leg Raise": {"up": 60, "down": 10},
-}
+exercise_flag = "Push-up" 
+pushup_counter = 0
+pushup_status = False
+squat_counter = 0
+squat_status = False
+bicep_curl_counter = 0
+bicep_curl_status = False
+exercise_threshold = 5  
 
 def calculate_angle(a, b, c):
-    a = np.array(a)
-    b = np.array(b)
-    c = np.array(c)
-    
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
-    
-    if angle > 180.0:
-        angle = 360 - angle
-        
-    return angle
+    ab = np.array(b) - np.array(a)
+    bc = np.array(c) - np.array(b)
+    dot_product = np.dot(ab, bc)
+    magnitude_ab = np.linalg.norm(ab)
+    magnitude_bc = np.linalg.norm(bc)
+    cosine_angle = dot_product / (magnitude_ab * magnitude_bc)
+    angle = np.arccos(cosine_angle)
+    return angle * 180.0 / np.pi
 
-def classify_exercise(landmarks):
-    # Calculate the angles for both exercises
-    bicep_curl_angle = calculate_angle(
-        [landmarks["Bicep Curl"]["shoulder"].x, landmarks["Bicep Curl"]["shoulder"].y],
-        [landmarks["Bicep Curl"]["elbow"].x, landmarks["Bicep Curl"]["elbow"].y],
-        [landmarks["Bicep Curl"]["wrist"].x, landmarks["Bicep Curl"]["wrist"].y]
-    )
-
-    leg_raise_angle = calculate_angle(
-        [landmarks["Leg Raise"]["hip"].x, landmarks["Leg Raise"]["hip"].y],
-        [landmarks["Leg Raise"]["knee"].x, landmarks["Leg Raise"]["knee"].y],
-        [landmarks["Leg Raise"]["ankle"].x, landmarks["Leg Raise"]["ankle"].y]
-    )
-
-    # Determine the exercise based on angles
-    if bicep_curl_angle > angle_thresholds["Bicep Curl"]["down"]:
-        return "Bicep Curl"
-    elif leg_raise_angle > angle_thresholds["Leg Raise"]["down"]:
-        return "Leg Raise"
-    else:
-        return "Unknown"
+def detection_body_part(landmarks, body_part_name):
+    return [
+        landmarks[mp_pose.PoseLandmark[body_part_name].value].x,
+        landmarks[mp_pose.PoseLandmark[body_part_name].value].y,
+        landmarks[mp_pose.PoseLandmark[body_part_name].value].visibility
+    ]
 
 cap = cv2.VideoCapture(0)
 
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    exercise_type = None
-    counter = 0
-    stage = None
-
     while cap.isOpened():
         ret, frame = cap.read()
-        
-        # Recolor image to RGB
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
-      
-        # Make detection
         results = pose.process(image)
-    
-        # Recolor back to BGR
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # Extract landmarks for both exercises
-        landmarks = {}
-        for exercise, points in exercise_landmarks.items():
-            landmarks[exercise] = {}
-            for point, landmark in points.items():
-                landmarks[exercise][point] = results.pose_landmarks.landmark[landmark.value]
 
-        # Dynamically classify the exercise
-        exercise_type = classify_exercise(landmarks)
-        
-        # Visualize exercise type
-        cv2.putText(image, f"Exercise: {exercise_type}", (10, 30), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
+        try:
+            landmarks = results.pose_landmarks.landmark
 
-        if exercise_type != "Unknown":
-            # Calculate and count reps for the detected exercise
-            try:
-                points = exercise_landmarks[exercise_type]
-                a = [landmarks[exercise_type][points["shoulder"]].x, landmarks[exercise_type][points["shoulder"]].y]
-                b = [landmarks[exercise_type][points["elbow"]].x, landmarks[exercise_type][points["elbow"]].y]
-                c = [landmarks[exercise_type][points["wrist"]].x, landmarks[exercise_type][points["wrist"]].y]
+            if exercise_flag == "Push-up":
+                left_shoulder = detection_body_part(landmarks, "LEFT_SHOULDER")
+                left_elbow = detection_body_part(landmarks, "LEFT_ELBOW")
+                left_wrist = detection_body_part(landmarks, "LEFT_WRIST")
 
-                # Calculate angle
-                angle = calculate_angle(a, b, c)
+                # Calculate the angle of the left arm
+                left_arm_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
 
-                # Visualize angle
-                cv2.putText(image, str(angle), 
-                           tuple(np.multiply(b, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+                # Push-up counter logic
+                if not pushup_status and left_arm_angle > 150:
+                    pushup_status = True
+                elif pushup_status and left_arm_angle < 30:
+                    pushup_counter += 1
+                    pushup_status = False
+                    if pushup_counter >= exercise_threshold:
+                        exercise_flag = "Squat"
+                        pushup_counter = 0
 
-                # Curl counter logic"
-                if angle > angle_thresholds[exercise_type]["down"]:
-                    stage = "down"
-                if angle < angle_thresholds[exercise_type]["up"] and stage =='down':
-                    stage = "up"
-                    counter += 1
-                    print(f"Exercise: {exercise_type}, Reps: {counter}")
+            elif exercise_flag == "Squat":
+                left_hip_angle = calculate_angle(detection_body_part(landmarks, "LEFT_SHOULDER"),
+                                                 detection_body_part(landmarks, "LEFT_HIP"),
+                                                 detection_body_part(landmarks, "LEFT_KNEE"))
+                right_hip_angle = calculate_angle(detection_body_part(landmarks, "RIGHT_SHOULDER"),
+                                                  detection_body_part(landmarks, "RIGHT_HIP"),
+                                                  detection_body_part(landmarks, "RIGHT_KNEE"))
+                if not squat_status and (left_hip_angle < 100 or right_hip_angle < 100):
+                    squat_status = True
+                elif squat_status and (left_hip_angle > 160 and right_hip_angle > 160):
+                    squat_counter += 1
+                    squat_status = False
+                    if squat_counter >= exercise_threshold:
+                        exercise_flag = "Bicep Curl"
+                        squat_counter = 0
 
-            except:
-                pass
+            elif exercise_flag == "Bicep Curl":
+                left_shoulder = detection_body_part(landmarks, "LEFT_SHOULDER")
+                left_elbow = detection_body_part(landmarks, "LEFT_ELBOW")
+                left_wrist = detection_body_part(landmarks, "LEFT_WRIST")
 
-        # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
-        
-        # Rep data
-        cv2.putText(image, 'REPS', (15,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter), 
-                    (10,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-        
-        # Stage data
-        cv2.putText(image, 'STAGE', (65,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage, 
-                    (60,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-        
-        # Render detections
+                # Calculate the angle of the left arm
+                left_arm_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+
+                # Bicep curl counter logic
+                if not bicep_curl_status and left_arm_angle > 150:
+                    bicep_curl_status = True
+                elif bicep_curl_status and left_arm_angle < 30:
+                    bicep_curl_counter += 1
+                    bicep_curl_status = False
+
+        except:
+            pass
+
+        cv2.rectangle(image, (0, 0), (225, 120), (245, 117, 16), -1)
+
+        cv2.putText(image, 'EXERCISE', (15, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, exercise_flag if exercise_flag else "None",
+                    (10, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, 'PUSH-UP REPS', (15, 70),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(pushup_counter),
+                    (10, 100),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, 'SQUAT REPS', (15, 130),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(squat_counter),
+                    (10, 160),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, 'BICEP CURL REPS', (15, 190),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(bicep_curl_counter),
+                    (10, 220),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                 )               
-        
+                                   mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                   mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+
         cv2.imshow('Mediapipe Feed', image)
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
